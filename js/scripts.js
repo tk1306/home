2edfe9e22956b82c077f5819fd67a977e3c879e2
tk1306 <krephetony@gmail.com>
2022-03-22 17:51:45 -0300

    bn

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,4 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+<a href="https://www.webcontadores.com" title="contador de visitas tumblr"><img src="https://counter9.stat.ovh/private/webcontadores.php?c=gmdlrjgddp74y3dh85jqfyp7xub1e9sq" border="0" title="contador de visitas tumblr" alt="contador de visitas tumblr"></a>
